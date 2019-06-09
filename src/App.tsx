83b2849e743d90ab6cf5140652655f05beced5c0
LeoCantThinkOfAName <i7851@hotmail.com>
2019-06-09 19:16:43 +0800

    add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from "react";
 // context provider
 import { ContextProvider } from "./context/context.composer";
 
+// i18n
+import "./i18n/i18n";
+
 // styles
 import "./styles/baseStyle.scss";
 
